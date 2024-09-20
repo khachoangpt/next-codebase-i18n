cbd81afac9d5969d50b3da1c071c11a6a73aed1c
Nguyen Hoang <nguyenhoang@Nguyens-Laptop.local>
2024-09-20 17:32:07 +0700

    fix: remove file for i18n, fix fetch swagger

--- src/helpers/fetch-openapi.ts
+++ src/helpers/fetch-openapi.ts
@@ -3,11 +3,26 @@ import '@/configs/env'
 import { logger } from '@/configs/logger'
 import { generateOpenApiSpecification } from '@/utils/generate-openapi-specs'
 
+/**
+ * Fetches OpenAPI specification from the given URL and generates the necessary
+ * TypeScript definitions and API clients.
+ *
+ * This function is called by the `fetch-openapi` script and is used to generate
+ * the API clients and TypeScript definitions for the Next.js API.
+ *
+ * The URL is read from the `SWAGGER_JSON_URL` environment variable. If the
+ * variable is not set, the function logs an error and does nothing.
+ *
+ * The function fetches the OpenAPI specification from the given URL, parses it
+ * as JSON and then passes it to the `generateOpenApiSpecification` function.
+ *
+ * If the fetching fails, the function logs an error and throws an exception.
+ */
 const fetchOpenApi = async () => {
   const swaggerURL = process.env.SWAGGER_JSON_URL
   if (!swaggerURL) {
-    logger.warn('SWAGGER_JSON_URL is not set')
-    return
+    logger.error('SWAGGER_JSON_URL is not set')
+    throw new Error('SWAGGER_JSON_URL is not set')
   }
 
   try {
@@ -15,8 +30,13 @@ const fetchOpenApi = async () => {
     const swaggerJson = await response.json()
     logger.info(`Fetched OpenAPI specification :: ${swaggerURL}`)
     await generateOpenApiSpecification(swaggerJson)
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
   } catch (error) {
-    logger.warn(`Failed to fetch OpenAPI specification :: ${error}`)
+    const errorMessage =
+      'Failed to fetch OpenAPI specification. Make sure you are setting SWAGGER_JSON_URL correctly.'
+
+    logger.error(errorMessage)
+    throw new Error(errorMessage)
   }
 }
 
--- src/utils/format-number.ts
+++ src/utils/format-number.ts
@@ -1,3 +1,14 @@
-export const formatNumber = (num: number | string) => {
+/**
+ * Format a number by inserting commas as thousand separators.
+ *
+ * @example
+ * formatNumber(1000) // '1,000'
+ * formatNumber(123456) // '123,456'
+ * formatNumber(1234567890) // '1,234,567,890'
+ *
+ * @param {number | string} num - The number to format.
+ * @returns {string} The formatted number.
+ */
+export const formatNumber = (num: number | string): string => {
   return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
 }
